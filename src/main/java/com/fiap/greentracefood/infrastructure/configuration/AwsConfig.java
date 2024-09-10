4006959dd4caa21c551f59ab0bf85ca187dc152e
fabricioguimaraesdeoliveira <fabricio.oliveira@partner.ideen.tech>
2024-09-09 22:55:45 -0400

    Implementando deploy EKS

--- src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
+++ src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
@@ -1,13 +1,9 @@
 package com.fiap.greentracefood.infrastructure.configuration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-
-import java.net.URI;
 
 @Configuration
 public class AwsConfig {
@@ -15,9 +11,9 @@ public class AwsConfig {
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
                 .region(Region.of("us-east-1"))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
+
 }
--- src/main/java/com/fiap/greentracefood/infrastructure/configuration/DynamoDbConfiguration.java
+++ src/main/java/com/fiap/greentracefood/infrastructure/configuration/DynamoDbConfiguration.java
@@ -6,7 +6,6 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-import java.net.URI;
 
 @Configuration
 public class DynamoDbConfiguration {
@@ -16,7 +15,6 @@ public class DynamoDbConfiguration {
     public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
         return DynamoDbEnhancedClient.builder()
                 .dynamoDbClient(DynamoDbClient.builder()
-                        .endpointOverride(URI.create("http://localhost:4566"))
                         .region(Region.US_EAST_1)
                         .build())
                 .build();
